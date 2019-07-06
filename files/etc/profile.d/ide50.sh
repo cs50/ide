0c66cf0574914e035198b0d30363aa6cddeab396
David J. Malan <malan@harvard.edu>
2019-07-06 18:50:17 -0400

    Relocated lines to cs50/baseimage or cs50/cli, or removed as redundant already

--- files/etc/profile.d/ide50.sh
+++ files/etc/profile.d/ide50.sh
@@ -1,23 +1,6 @@
 # If not root
 if [ "$(id -u)" != "0" ]; then
 
-    # Set umask
-    umask 0077
-
-    # Configure clang
-    export CC="clang"
-    export CFLAGS="-fsanitize=signed-integer-overflow -fsanitize=undefined -ggdb3 -O0 -std=c11 -Wall -Werror -Wextra -Wno-sign-compare -Wno-unused-parameter -Wno-unused-variable -Wshadow"
-    export LDLIBS="-lcrypt -lcs50 -lm"
-
-    # Protect user
-    alias cp="cp -i"
-    alias ls="ls --color=auto"
-    alias mv="mv -i"
-    alias rm="rm -i"
-
-    # Suppress gdb's startup output
-    alias gdb="gdb -q"
-
     # Shift out and in of block character palettes
     alias break50="printf '\x0e'"
     alias fix50="printf '\x0f'"
@@ -29,69 +12,12 @@ ulimit -f 524288
 # Disable auto-logout
 export TMOUT="0"
 
-# Java
-export CLASSPATH=".:/usr/share/java/cs50.jar"
-
 # c9 open
 alias open="c9 open"
-
-# python3
-alias pip="pip3"
-alias pylint="pylint3"
-alias python="python3"
-export PYTHONDONTWRITEBYTECODE="1"
-
-# sqlite3
-alias sqlite3="sqlite3 -column -header"
-
-# sudo
-# Trailing space enables elevated command to be an alias
-alias sudo="sudo "
-
-# flask
-export FLASK_APP="application.py"
-export FLASK_DEBUG="0"
-export FLASK_ENV="development"
-
-# valgrind defaults
-export VALGRIND_OPTS="--memcheck:leak-check=full --memcheck:track-origins=yes"
-
-# Set editor
-export EDITOR="nano"
 
 # Set locale
 export LANG="C"
 
-# Node.js
-export NODE_ENV="dev"
-
-# Web application environment
-export APPLICATION_ENV="dev"
-
 # Short-circuit RVM's cd script
 # https://news.ycombinator.com/item?id=1637354
 export rvm_project_rvmrc="0"
-
-# History
-# https://www.shellhacks.com/tune-command-line-history-bash/
-export PROMPT_COMMAND='history -a'  # Store Bash History Immediately
-
-# Ensure no make targets end with .c
-make () {
-    local args=""
-    local invalid_args=0
-
-    for arg; do
-        case "$arg" in
-            (*.c) arg=${arg%.c}; invalid_args=1;;
-        esac
-        args="$args $arg"
-    done
-
-    if [ $invalid_args -eq 1 ]; then
-        echo "Did you mean 'make$args'?"
-        return 1
-    else
-        command make -B $*
-    fi
-}
