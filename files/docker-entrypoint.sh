409488302beb614de4bf03aa1b7c198080fe28e6
Kareem Zidane <kzidane@cs50.harvard.edu>
2020-09-29 19:27:27 -0400

    remove sudo access

--- files/docker-entrypoint.sh
+++ files/docker-entrypoint.sh
@@ -5,4 +5,7 @@ set -e
 echo "starting rsyslog..."
 sudo service rsyslog start
 
+echo "removing sudo access..."
+sudo sed -i '/^ubuntu ALL=(ALL) NOPASSWD:ALL$/d' /etc/sudoers
+
 cd /opt/c9/packages/cs50 && npm run standalone
