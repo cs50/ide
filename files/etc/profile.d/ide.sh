068062de8c68f44f1dd67e9df3076a11ecaf0219
David J. Malan <malan@harvard.edu>
2020-12-21 22:13:43 -0500

    readying for focal, new wrappers

--- files/etc/profile.d/ide.sh
+++ files/etc/profile.d/ide.sh
@@ -1,61 +1,43 @@
 # If not root
-if [ "$(id -u)" != "0" ]; then
+if [ "$USER" != "root" ]; then
 
     # Shift out and in of block character palettes
     alias break50="printf '\x0e'"
     alias fix50="printf '\x0f'"
 fi
 
-# Set maximum file size to 512MB
-ulimit -Sf 524288
-
-
-alias cd="HOME=$HOME/workspace cd"
-alias ls="ls --color -F --ignore=lost+found"
-
-alias open="c9 open"
+# Aliases
 alias ddb="c9 exec ddb"
+alias open="c9 open"
 
+# Assume online by default
 if [ -z "$CS50_IDE_TYPE" ]; then
     export CS50_IDE_TYPE="online"
 fi
 
-export DISPLAY=":0"
-
-# Editor
-export EDITOR="nano"
-
-# Localization
-export LANG="C.UTF-8"
-export LC_ALL="C.UTF-8"
-export LC_CTYPE="C.UTF-8"
-
-# Java
-export CLASSPATH=".:/usr/share/java/cs50.jar"
-export JAVA_HOME="/opt/jdk-12.0.1"
-
-# Node
-export NODE_ENV="dev"
-
-# Flask
-export FLASK_APP="application.py"
-export FLASK_DEBUG="0"
-export FLASK_ENV="development"
-
-export APPLICATION_ENV="dev"
-
-# Python
-export PATH="$HOME"/.local/bin:"$PATH"
-export PYTHONDONTWRITEBYTECODE="1"
-
-# Ruby
-export GEM_HOME="$HOME"/.gem
-export PATH="$GEM_HOME"/bin:"$PATH"
-
 # check50
 export CHECK50_WORKERS=1
+
+# For development
+export APPLICATION_ENV="dev"
 
 # Prevent Ctrl-S from freezing terminal in interactive mode
 case $- in
     *i*) stty -ixon ;;
 esac
+
+# Set maximum file size to 512MB
+ulimit -Sf 524288
+
+# Wrappers
+URL="(http://)[0-9]+\.[0-9]+\.[0-9]+\.[0-9]:(\x1b\[[0-9;]*m)?[0-9]+(\x1b\[[0-9;]*m)?(\S+)"
+flask() {
+    unbuffer /opt/cs50/bin/flask "$@" | sed -E "s#${URL}#\1TODO\4#"
+}
+
+http_server() {
+    unbuffer /opt/cs50/bin/http-server "$@" | unbuffer -p sed -E "s#${URL}#\1TODO\4#" | uniq
+}
+
+# X Window System
+export DISPLAY=":0"
